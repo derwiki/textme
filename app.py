135f1792a662fbc76573b4058a3e4b19852e0732
Adam Derewecki <derewecki@gmail.com>
2020-05-07 21:25:45 -0700

    black .

--- app.py
+++ app.py
@@ -5,16 +5,14 @@ from twilio.rest import Client
 app = Flask(__name__)
 
 
-TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
-TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID", None)
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN", None)
 CLIENT = Client(TWILIO_ACCOUNT_SID, TWILIO_ACCOUNT_SID)
 
 
-@app.route('/')
+@app.route("/")
 def textme():
     CLIENT.Messages.create(
-      body=request.values['t'],
-      from_='6145052620',
-      to='7405023073'
+        body=request.values["t"], from_="6145052620", to="7405023073"
     )
-    return 'OK'
+    return "OK"
