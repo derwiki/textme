00637181244345757835ece74493a2be3753a072
Adam Derewecki <derewecki@gmail.com>
2020-05-11 17:14:05 -0700

    From/to numbers from env

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from twilio.rest import Client
 app = Flask(__name__)
 
 
+NUMBER_FROM = os.environ.get("NUMBER_FROM", None)
+NUMBER_TO = os.environ.get("NUMBER_TO", None)
 TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID", None)
 TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN", None)
 CLIENT = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
@@ -13,5 +15,5 @@ CLIENT = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 @app.route("/")
 def textme():
     body = request.values.get("t", "SYN")
-    CLIENT.messages.create(body=body, from_="6145052620", to="7405023073")
+    CLIENT.messages.create(body=body, from_=NUMBER_FROM, to=NUMBER_FROM)
     return "OK"
